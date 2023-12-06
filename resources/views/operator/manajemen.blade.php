f7952aa150e485a5a0afb13b1c8d953cb7f23c0b
Mulyairwansyah15 <mulyairwansyah15@gmail.com>
2023-12-07 02:46:50 +0700

    benerin tampilan

--- resources/views/operator/manajemen.blade.php
+++ resources/views/operator/manajemen.blade.php
@@ -14,9 +14,6 @@
                 <span class="font-medium">{{ session('success') }}</span>
             </div>      
         @else
-        <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-            <span class="font-medium">{{ session('error') }}</span>
-        </div>
         @endif
        </div>          
         <div class="relative overflow-x-auto">
