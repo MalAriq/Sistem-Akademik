87bf67682fa5ec3ad129f8922143204cfd286136
erlanig <erlanig@students.undip.ac.id>
2023-12-06 22:49:35 +0700

    departemen button cetak file

--- resources/views/departemen/sudahSkripsi.blade.php
+++ resources/views/departemen/sudahSkripsi.blade.php
@@ -8,7 +8,7 @@
            <p class="text-3xl font-bold text-gray-900 dark:text-white uppercase">Data Sudah Skripsi Mahasiswa Angkatan {{$tahun}}</p>
         </div>
         <div>
-            <a href="{{ route('departemen.cetaksudahskripsi', ['tahun' => $tahun]) }}" class="text-white bg-blue-500 hover:bg-blue-600 font-medium text-base text-center py-2 px-4 rounded-full" target="_blank">Cetak Sudah Skripsi</a>
+            <a href="{{ route('departemen.cetaksudahskripsi', ['tahun' => $tahun]) }}" class="text-white bg-blue-500 hover:bg-blue-600 font-medium text-base text-center py-2 px-4 rounded-full" target="_blank">Cetak File</a>
             <table class="mt-3" style="width: 60%;">
                 <tr>
                     <td style="border: none;">Nama Dosen Wali</td>
